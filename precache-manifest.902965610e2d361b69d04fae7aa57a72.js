9c38461d0068e7cdf2a778c2948f241bd0533f03
Kinga Tokarzewska <kingatok@gmail.com>
2018-10-07 17:03:42 +0200

    Updates

---
+++ precache-manifest.902965610e2d361b69d04fae7aa57a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df98274e1bba5f80a589",
+    "url": "/static/js/main.df98274e.chunk.js"
+  },
+  {
+    "revision": "e4edf6ec78ac3e6ed0f4",
+    "url": "/static/js/1.e4edf6ec.chunk.js"
+  },
+  {
+    "revision": "df98274e1bba5f80a589",
+    "url": "/static/css/main.26c5f182.chunk.css"
+  },
+  {
+    "revision": "79147765e0a7d9e9e7cd83bb737eead9",
+    "url": "/index.html"
+  }
+];
